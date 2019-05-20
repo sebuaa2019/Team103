571554ab8909a0eba91364d52a2b9dd3748dd668
ChenQixian <chenqixian@buaa.edu.cn>
2019-05-20 22:57:03 +0800

    push xianxian's work

--- team103V2.0/src/team103_main.cpp
+++ team103V2.0/src/team103_main.cpp
@@ -1,36 +1,3 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2017-2020, Waterplus http://www.6-robot.com
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the WaterPlus nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  FOOTPRINTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
 /* @author team103                                                  */
 
 #include "gmain.h"
@@ -39,8 +6,9 @@
 #include "voice.h"
 #include "grab.h"
 #include "navigation.h"
+#include "unit_test.h"
 // namespace robot{
-// xianxian hao li hai
+// xianxian hao li hai X 2
 // }
 
 
@@ -67,6 +35,50 @@ int main(int argc, char** argv)
     InitKeyword();
 
     ROS_WARN("[main] team103");
+
+
+/* unit test
+    if (ros::ok())
+{
+    follow_test();
+
+    start_test();
+
+
+    FollowSwitch_test();
+
+    //Speak_test_callback(const sound_play::SoundRequest& res);
+
+    Speak_test();
+    AddNewWaypoint_test();
+
+    InitKeyword_test();
+
+    xxnav_test();
+
+    GrabSwitch_test();
+    PassSwitch_test();
+
+    xxgrab_test();
+
+    xxpass_test();
+
+    GrabResultCallback_test();
+
+    PassResultCallback_test();
+
+    FindKeyword_test();
+
+    Keyword_test_CB();
+
+    xxnav_test();
+}
+*/
+//*
+
+
+
+   
     ros::Rate r(30);
     while(ros::ok())
     {
@@ -108,6 +120,8 @@ int main(int argc, char** argv)
         ros::spinOnce();
         r.sleep();
     }
+
+    // */
 
     return 0;
 }
