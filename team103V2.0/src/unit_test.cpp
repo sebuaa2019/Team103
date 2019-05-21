c37f4ff4a16dc878543467a71fb1642c6c2ba586
ChenQixian <chenqixian@buaa.edu.cn>
2019-05-21 11:24:37 +0800

    cqx commit

---
+++ team103V2.0/src/unit_test.cpp
@@ -0,0 +1,44 @@
+#include "unit_test.h"
+#include <sstream>
+
+using namespace std;
+
+// @author :ChenQixian
+void follow_test() {
+    cout << "follow_test start..." << endl;
+    nDelay = 0;
+    follow();
+    assert(nDelay == 1);
+    cout << "follow_test done." << endl;    
+}
+
+
+void start_test() {
+    cout << "start_test start..." << endl;
+    nDelay = 99;
+    nState = STATE_READY;
+    start();
+    assert(nDelay == 100);
+    assert(nState == STATE_READY);
+    start();
+    assert(nDelay == 0);
+    assert(nState == STATE_FOLLOW);
+    cout << "start_test done." << endl;
+}
+
+
+
+// }
+void FollowSwitch_test() {
+    cout << "FollowSwitch_test start..." << endl;
+    bool inActive = true;
+    float inDist = 0.7;
+    FollowSwitch(inActive, inDist);
+    assert(srvFlw.request.thredhold == inDist);
+
+    inActive = false;
+    inDist = 0.5;
+    FollowSwitch(inActive, inDist);
+    assert(srvFlw.request.thredhold != inDist);
+    cout << "FollowSwitch_test done." << endl;
+}
