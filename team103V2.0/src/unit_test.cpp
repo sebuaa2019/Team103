1382e8866b1983e30b881513bb00f51bee4b5241
EurusQF <queenqin918@126.com>
2019-05-21 11:27:16 +0800

    Update unit_test.cpp

--- team103V2.0/src/unit_test.cpp
+++ team103V2.0/src/unit_test.cpp
@@ -9,7 +9,7 @@ void follow_test() {
     nDelay = 0;
     follow();
     assert(nDelay == 1);
-    cout << "follow_test done." << endl;    
+    cout << "follow_test done." << endl;
 }
 
 
@@ -41,4 +41,60 @@ void FollowSwitch_test() {
     FollowSwitch(inActive, inDist);
     assert(srvFlw.request.thredhold != inDist);
     cout << "FollowSwitch_test done." << endl;
+}
+//@author qinfeng
+void xxpass_test() {
+    cout << "xxpass_test start..." << endl;
+    nDelay = 0;
+    nState = STATE_PASS;
+    bPassDone = true;
+    xxpass();
+    assert(nDelay == 1);
+    assert(bPassDone == false);
+    assert(nState == STATE_PASS);
+
+    bPassDone = true;
+    xxpass();
+    assert(nDelay == 2);
+    assert(bPassDone == true);
+    assert(nState == STATE_ASK);
+    cout << "xxpass_test done." << endl;
+}
+
+
+void GrabResultCallback_test() {
+    cout << "GrabResultCallback_test start..." << endl;
+    ros::NodeHandle n;
+    ros::Publisher grab_result_pub = n.advertise<std_msgs::String>("/wpb_home/grab_result",30);
+    std_msgs::String msg;
+
+    msg.data = "foo";
+    bGrabDone = false;
+    grab_result_pub.publish(msg);
+    assert(bGrabDone == false);
+
+    msg.data = "done";
+    bGrabDone = false;
+    grab_result_pub.publish(msg);
+    assert(bGrabDone == true);
+    cout << "GrabResultCallback_test done." << endl;
+}
+
+
+void PassResultCallback_test() {
+    cout << "PassResultCallback_test start..." << endl;
+    ros::NodeHandle n;
+    ros::Publisher pass_result_pub = n.advertise<std_msgs::String>("/wpb_home/pass_result",30);
+    std_msgs::String msg;
+
+    msg.data = "foo";
+    bPassDone = false;
+    pass_result_pub.publish(msg);
+    assert(bPassDone == false);
+
+    msg.data = "done";
+    bPassDone = false;
+    pass_result_pub.publish(msg);
+    assert(bPassDone == true);
+    cout << "PassResultCallback_test done." << endl;
 }
