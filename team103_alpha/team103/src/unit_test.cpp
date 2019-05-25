edaa7f393bb2cdd16ad28d5e36507a0e3f13bbff
ll <ll1664594616@gmail.com>
2019-05-25 16:39:21 +0800

    test

--- team103_alpha/team103/src/unit_test.cpp
+++ team103_alpha/team103/src/unit_test.cpp
@@ -38,3 +38,39 @@ void FollowSwitch_test() {
     assert(srvFlw.request.thredhold != inDist);
     cout << "FollowSwitch_test done." << endl;
 }
+
+
+void Speak_test_callback(const sound_play::SoundRequest& res) {
+    assert(res.arg == "Speak_test");
+}
+void Speak_test() {
+    cout << "Speak_test start..." << endl;
+    ros::NodeHandle n;
+    ros::Subscriber spk_msg_test = n.subscribe("/robotsound", 20, &Speak_test_callback);
+    std::string str = "Speak_test";
+    Speak(str);
+    cout << "Speak_test done." << endl;
+}
+
+// @auther: liuliang
+void AddNewWaypoint_test_callback(const waterplus_map_tools::Waypoint& res) {
+    assert(res.name == "coffee");
+}
+void AddNewWaypoint_test() {
+    cout << "AddNewWaypoint_test start..." << endl;
+    ros::NodeHandle n;
+    ros::Subscriber AddNewWaypoint_test_sub = n.subscribe("/waterplus/add_waypoint", 1, &AddNewWaypoint_test_callback);
+    std::string str = "coffee";
+    AddNewWaypoint(str);
+    cout << "AddNewWayPoint_test done." << endl;
+}
+
+void InitKeyword_test() {
+    InitKeyword();
+    std::set<std::string> tmp(arKeyword.begin(), arKeyword.end());
+    assert(tmp.count("start"));
+    assert(tmp.count("water"));
+    assert(tmp.count("tea"));
+    assert(tmp.count("cola"));
+}
+
